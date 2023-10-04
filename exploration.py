2eb75e233cf8ff57c7abd75c68ad59a86a9824a0
HORIZON-Donut <kiettisak.k2546@gmail.com>
2023-10-05 01:58:08 +0700

    first commit

---
+++ exploration.py
@@ -0,0 +1,11 @@
+#simulate sensor data
+front_left = None
+front_right = None
+back_left = None
+back_right = None
+front = None
+
+#simulate object center data
+object_center_x = None
+object_center_y = None
+
